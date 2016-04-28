4ecc83277b61271cbb56cfee78e06e07e6784d5e
8am.jp <wakeup@8am.jp>
2016-04-28 22:44:16 +0900

    LED CLOCK

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*
- * Illuminometer
+ * LED CLOCK
  *
  * Copyright 2015 8am.
  * http://8am.jp/
@@ -35,7 +35,7 @@ $(function() {
         $(".separator").css("margin", Math.floor(cellwidth * 0.25));
         container.css("margin-top", (windowheight - container.height()) * 0.5);
     }
-    
+
     function play() {
         var d = new Date();
         var n = (('0' + d.getHours()).slice(-2) + ('0' + d.getMinutes()).slice(-2) + ('0' + d.getSeconds()).slice(-2)).split('');
@@ -45,7 +45,7 @@ $(function() {
 
         requestAnimationFrame(play);
     }
-    
+
     function init() {
         var classes = [
             "d5 d7",
@@ -53,62 +53,62 @@ $(function() {
             "d0 d1 d2 d3 d5 d6 d7 d8 d9",
             "d0 d2 d3 d4 d5 d6 d7 d8 d9",
             "d5 d7",
-    
+
             "d0 d2 d3 d5 d6 d8 d9",
             "d1",
             "d1 d4",
             "d4",
             "d0 d2 d3 d7 d8 d9",
-    
+
             "d0 d5 d6 d8 d9",
             "d4",
             "d1",
             "d4 d7",
             "d0 d2 d3 d8 d9",
-    
+
             "d0 d4 d5 d6",
             "d3 d5 d6 d8 d9",
             "d1 d3 d5 d6 d8 d9",
             "d2 d3 d4 d5 d6 d7 d8 d9",
             "d0 d9",
-    
+
             "d0 d4 d6 d8",
             "d4",
             "d1 d2 d4 d7",
             "d4",
             "d0 d3 d4 d5 d6 d8 d9",
-    
+
             "d0 d3 d6 d8",
             "d2",
             "d1 d7",
             "d4",
             "d0 d3 d5 d6 d8 d9",
-    
+
             "d2 d5",
             "d0 d2 d3 d5 d6 d8 d9",
             "d0 d1 d2 d3 d5 d6 d7 d8 d9",
             "d0 d2 d3 d4 d5 d6 d8 d9",
             "d2",
         ];
-    
+
         var dot = ($("<div/>").addClass("dot"));
         var cell = ($("<div/>").addClass("cell").append(dot.clone()));
-    
+
         var line = ($("<div/>").addClass("line"));
         for (i = 0; i < numOfHorizontalDots; i++) line.append(cell.clone());
         var digit = ($("<div/>").addClass("digit"));
         for (i = 0; i < numOfVerticalDots; i++) digit.append(line.clone());
-    
+
         var d = digit.find(".dot");
         for (i = 0; i < classes.length; i++) d.eq(i).addClass(classes[i]);
-    
+
         var separatorline = ($("<div/>").addClass("line"));
         separatorline.append(cell.clone());
         var separator = ($("<div/>").addClass("separator"));
         for (i = 0; i < numOfVerticalDots; i++) separator.append(separatorline.clone());
         var d = separator.find(".dot");
         $.each( [2, 4], function( key, value ) { d.eq(value).addClass("blink"); });
-    
+
         container = $("#led-clock");
         container.append(
             digit.clone(),
@@ -120,7 +120,7 @@ $(function() {
             digit.clone(),
             digit.clone()
         );
-    
+
         digits = [];
         $(".digit").each(function(i) {
             digits[i] = [];
